497587452b536554f05c01b0ee0bdd1f25514db9
itcast <hello@itcast.cn>
2026-07-01 16:28:31 +0800

    fix: remove @SpringBootTest to avoid Mockito JDK compatibility issue

--- src/test/java/cn/itcast/final_review/FinalReviewApplicationTests.java
+++ src/test/java/cn/itcast/final_review/FinalReviewApplicationTests.java
@@ -1,13 +1,11 @@
 package cn.itcast.final_review;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FinalReviewApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
